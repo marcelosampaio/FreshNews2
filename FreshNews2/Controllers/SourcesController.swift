61092b8ba2004373b8ed66277f26ccece06c2bfb
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-10-19 20:09:41 -0300

    initial commit

---
+++ FreshNews2/Controllers/SourcesController.swift
@@ -0,0 +1,54 @@
+//
+//  SourcesController.swift
+//  FreshNews2
+//
+//  Created by Marcelo on 19/10/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import UIKit
+
+class SourcesController: UITableViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var noContentView: UIView!
+    @IBOutlet var noContentLabel: UILabel!
+    
+    // MARK: - Properties
+    private var sources : SourcesViewModel!
+    private var dataSource : TableViewDataSource<SourceTableViewCell,SourceViewModel>!
+    private var cellIdentifier = "Cell"
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "News Feed"
+        self.tableView.estimatedRowHeight = CGFloat(190)
+        self.tableView.rowHeight = UITableView.automaticDimension
+        // load app data
+        loadAppData()
+    }
+    
+    // MARK: App Data Source
+    private func loadAppData() {
+        self.sources = SourcesViewModel(completion: {
+            // completion
+            self.dataSource = TableViewDataSource(cellIdentifier: self.cellIdentifier, items: self.sources.sources, configureCell: { (cell, vm) in
+                // completion
+            
+//                cell.sourcesTitle.text = vm.name
+//                cell.sourcesDescription.text = vm.description
+                cell.sourceName.text = "News Provider"
+                cell.sourceDescription.text = vm.name
+                cell.sourceDescription.text = vm.description
+            
+                
+            })
+            self.tableView.dataSource = self.dataSource
+            self.tableView.reloadData()
+        })
+        
+    }
+
+}
---
+++ FreshNews2/Service API/WebService.swift
@@ -0,0 +1,56 @@
+//
+//  WebService.swift
+//  FreshNews2
+//
+//  Created by Marcelo on 19/10/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import Foundation
+
+
+class WebService {
+    
+    // MARK: - Data Retrieval
+    func getNewsSources(completion: @escaping (WebContent) -> ()) {
+        // get url
+        let url = URL(string: WebService.getEndpoint("getSources"))
+        
+        // request web service
+        URLSession.shared.dataTask(with: url!) {
+            data, response, error in
+            if let data = data {
+                let json = try! JSONSerialization.jsonObject(with: data, options: [])
+                let dataDic : NSDictionary = json as! NSDictionary
+                DispatchQueue.main.async {
+                    completion(WebContent(dictionary: dataDic))
+                }
+            }
+            }.resume()
+    }
+    
+    
+    
+    // MARK: - Class Helper
+    class func getEndpoint(_ identifier: String) -> String {
+        
+        var result = String()
+        
+        if let path = Bundle.main.path(forResource: "Config", ofType: "plist") {
+            
+            // file root is a dictionary
+            if let dic = NSDictionary(contentsOfFile: path) as? [String: Any] {
+                print ("****** dic = \(dic)")
+                
+                result = dic[identifier] as! String
+                
+                
+            }
+        }
+        print("***2*** ACTION: \(identifier)")
+        return result
+    }
+    
+    
+}
+
