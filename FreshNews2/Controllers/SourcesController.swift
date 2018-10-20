9c634f5af1988e2a474a59cd01c63ea16d23bf77
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-10-20 13:30:30 -0300

    checking response's status code and error on WebService class

--- FreshNews2/Controllers/SourcesController.swift
+++ FreshNews2/Controllers/SourcesController.swift
@@ -26,30 +26,50 @@ class SourcesController: UITableViewController {
         self.navigationItem.title = "News Feed"
         self.tableView.estimatedRowHeight = CGFloat(190)
         self.tableView.rowHeight = UITableView.automaticDimension
+        self.setNoContent(msg: "Loading...")
+        self.setActivityIndicator(show: true)
         // load app data
         loadAppData()
     }
     
-    // MARK: App Data Source
+    // MARK: - App Data Source
     private func loadAppData() {
         self.sources = SourcesViewModel(completion: {
             // completion
             self.dataSource = TableViewDataSource(cellIdentifier: self.cellIdentifier, items: self.sources.sources, configureCell: { (cell, vm) in
                 // completion
-            
-//                cell.sourcesTitle.text = vm.name
-//                cell.sourcesDescription.text = vm.description
                 cell.sourceName.text = vm.name
                 cell.sourceDescription.text = vm.description
                 cell.sourceCategory.text = vm.category
                 cell.sourceDate.text = vm.country
-            
-                
             })
             self.tableView.dataSource = self.dataSource
+            if self.sources.sources.count == 0 {
+                self.setNoContent(msg: "No news!")
+            }else{
+                self.tableView.tableHeaderView = nil
+            }
+            self.setActivityIndicator(show: false)
             self.tableView.reloadData()
         })
         
     }
 
+    // MARK: - TableView Helper
+    private func setNoContent(msg: String) {
+        self.noContentLabel.text = msg
+        self.noContentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.bounds.size.height)
+        self.tableView.tableHeaderView = self.noContentView
+    }
+    
+    // MARK: - Activity Indicator
+    private func setActivityIndicator(show: Bool) {
+        self.activityIndicator.isHidden = !show
+        if show {
+            self.activityIndicator.startAnimating()
+        }else{
+            self.activityIndicator.stopAnimating()
+        }
+    }
+    
 }
